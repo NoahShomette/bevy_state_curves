36a4faf1e6bdea20e303dfd46ed672fea0c64a7c
Noah <noahshomette@gmail.com>
2023-12-10 11:27:51 -0500

    Remove unneeded bevy features, remove unused resources and components that are not part of the crate plan

--- crates/bevy_state_curves/src/lib.rs
+++ crates/bevy_state_curves/src/lib.rs
@@ -77,31 +77,10 @@
 mod curves;
 mod keyframe_trait;
 
-use bevy::{
-    prelude::{Component, Entity, Resource},
-    utils::HashMap,
-};
-
 pub mod prelude {
     pub use super::curves::{CurveTrait, LinearCurve, PulseCurve, SteppedCurve};
     pub use super::keyframe_trait::{LinearKeyframe, PulseKeyframe, SteppedKeyframe};
     pub use super::GameTick;
-}
-
-/// An id that represents a single objects state
-#[derive(Component)]
-struct ObjectId(pub u32);
-
-/// A resource that contains when each StateId existed
-#[derive(Resource)]
-struct ObjectTimings {
-    object_timings: HashMap<GameTick, Vec<ObjectId>>,
-}
-
-/// A resource that contains a map from an [`ObjectId`] to an entity that holds all of that [`ObjectId`] curves
-#[derive(Resource)]
-struct ObjectStateCurveMap {
-    object_state: HashMap<ObjectId, Entity>,
 }
 
 pub type GameTick = u64;
